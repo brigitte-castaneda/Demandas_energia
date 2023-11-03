a3426c2c121ecb3b637aa8b7932c114716d03e5a
Brigittecasro1 <k.castaneda@uniandes.edu.co>
2023-11-03 18:06:37 -0500

    COMPILO TABLAS

--- scripts/Transporte.R
+++ scripts/Transporte.R
@@ -18,7 +18,7 @@ getwd()
 
 #load data
 Servicios_del_hogar <- read_delim("ECV/Servicios del hogar/Servicios del hogar.csv", 
-                                  +     delim = ";", escape_double = FALSE, trim_ws = TRUE)
+                                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 Fuerza_de_trabajo <- read_delim("ECV/Fuerza de trabajo/Fuerza de trabajo.csv", 
                                 delim = ";", escape_double = FALSE, trim_ws = TRUE)
@@ -57,4 +57,91 @@ Educacion = Educacion[,c(1:3, 24, 25)]
 #SECUENCIA_ENCUESTA identifica el número de hogar dentro de la vivienda. 
 #SECUENCIA_P identifica a la persona dentro del hogar.
 
+#--compilo tabla----------------------------------------------------------------
+#creo llaves
+Servicios_del_hogar$key_DIR_E = paste(Servicios_del_hogar$DIRECTORIO, Servicios_del_hogar$SECUENCIA_ENCUESTA, sep = "_") 
+#Llave entre personas
+Fuerza_de_trabajo$key_DIR_P_E = paste(Fuerza_de_trabajo$DIRECTORIO, Fuerza_de_trabajo$SECUENCIA_P, Fuerza_de_trabajo$SECUENCIA_ENCUESTA, sep = "_") 
+Educacion$key_DIR_P_E = paste(Educacion$DIRECTORIO, Educacion$SECUENCIA_P, Educacion$SECUENCIA_ENCUESTA, sep = "_") 
+
+#Uno las tablas de personas
+resultado <- merge(Fuerza_de_trabajo, Educacion[,-c(1:3)], by = "key_DIR_P_E")
+
+#creo llave
+resultado$key_DIR_E = paste(resultado$DIRECTORIO, resultado$SECUENCIA_P, sep = "_") 
+
+#Uno las tablas de hogar y personas
+resultado_H_P <- merge(Servicios_del_hogar, resultado[,-c(2:4)], by = "key_DIR_E")
+
 #crear quintiles segun ingreso
+quintiles <- quantile(resultado_H_P$I_HOGAR , probs = seq(0, 1, 0.2))
+
+resultado_H_P$quintil <- cut(resultado_H_P$I_HOGAR, breaks = quintiles, labels = c("Q1", "Q2", "Q3", "Q4", "Q5"))
+hist(resultado_H_P$CANT_PERSONAS_HOGAR)
+
+# Cambiar la variable "mi_variable" a tipo character
+resultado_H_P$quintil <- as.character(resultado_H_P$quintil)
+
+#--------------------reviso NAS-----------------------------------
+
+
+#------------------graficas de analisis-----------------------------------------
+# Libraries
+library(tidyverse)
+library(hrbrthemes)
+library(viridis)
+library(ggplot2)
+
+# Plot ingreso
+resultado_H_P %>%
+  ggplot(aes(x=quintil, y=I_HOGAR, fill=quintil)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("Ingreso por quintiles") +
+  xlab("")
+
+
+# Plot ingreso
+resultado_H_P %>%
+  ggplot(aes(x=quintil, y=I_HOGAR, fill=quintil)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("Ingreso por quintiles") +
+  xlab("")
+
+
+# Plot CANTIDAD PERSONAS POR HOGAR
+resultado_H_P %>%
+  ggplot(aes(x=quintil, y=CANT_PERSONAS_HOGAR , fill=quintil)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("Ingreso por quintiles") +
+  xlab("")
+
+# Plot P6167
+resultado_H_P %>%
+  ggplot(aes(x=quintil, y=P6167 , fill=quintil)) +
+  geom_boxplot() +
+  scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("Ingreso por quintiles") +
+  xlab("")
